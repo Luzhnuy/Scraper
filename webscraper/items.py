f84b32ae086c8f90d51486fba280d2dbfc6b09ab
lubas <l.luzhnuy@gmail.com>
2019-07-07 18:23:13 +0300

    releaze

--- webscraper/items.py
+++ webscraper/items.py
@@ -4,11 +4,3 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
-
-
-class WebscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
--- webscraper/spiders/yellowpagespider.py
+++ webscraper/spiders/yellowpagespider.py
@@ -2,10 +2,11 @@ import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+
 class YellowPagesSpider(CrawlSpider):
     name = "yp"
     start_urls = [
-        "https://www.yellowpages.com/austin-tx/plumbers?",
+        "https://www.yellowpages.com/austin-tx/plumbers",
         # "https://www.yellowpages.com/austin-tx/plumbers?page=2"
     ]
     rules = (
@@ -16,15 +17,34 @@ class YellowPagesSpider(CrawlSpider):
         return self.parse_page(response)
 
     def parse_page(self, response):
-        # print(response.css('div.search-results.organic'))
-        print("1---------------------------------------------------1")
+
         for item in response.css('div.result'):
-            # print(item)
-            print('-------------------------------------------------')
             links = item.css('div.links')
             links = re.sub(r'\<[^>]*\>', '', str(links.extract()))
-            print(links.encode('utf-8'))
-            if "Directions" in links:
-                title = item.css('div.info>h2.n')
-                title = re.sub(r'\<[^>]*\>', '', str(title.extract()))
-                print(title.encode('utf-8'))
+            title = item.css('div.info>h2.n')
+            title = re.sub(r'\<[^>]*\>', '', str(title.extract()[0]))
+            if "Directions" in links and title[0].isdigit():
+                phone = item.css('div.phones')
+                try:
+                    phone = re.sub(r'\<[^>]*\>', '', str(phone.extract()[0]))
+                    phone = re.findall(r'[0-9]',phone)
+                    phone = ''.join(phone)
+                    street = item.css("div.street-address")
+                    location = item.css('div.locality')
+                    street = re.sub(r'\<[^>]*\>', '', str(street.extract()[0]))
+                    location = re.sub(r'\<[^>]*\>', '', str(location.extract()[0]))
+                    location = street + location
+                except:
+                    pnone = ""
+                title = re.sub(r'[0-9.]','', title)
+                print(title)
+                print(phone)
+                print(location)
+                title = title.replace('\u00a0','')
+                scraped_data = {
+                    title : {
+                    'phone': phone,
+                    'location':location,
+                    }
+                }
+                yield scraped_data
